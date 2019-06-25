142db1ca6024e03454f8bc5699186ed50a9c6841
Austin Phillips <cochapel@live.unc.edu>
2019-06-25 17:41:01 -0400

    First Run

---
+++ precache-manifest.c15396001de62f63601d4282e05ad6cd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce5e49d5a99f80d72f284e6e245a8b2",
+    "url": "/austinPortfolio/index.html"
+  },
+  {
+    "revision": "d4467927ad2668ca434a",
+    "url": "/austinPortfolio/static/css/main.d4fe2388.chunk.css"
+  },
+  {
+    "revision": "37df38bdccf4bce2f4d9",
+    "url": "/austinPortfolio/static/js/2.b6f9a99f.chunk.js"
+  },
+  {
+    "revision": "d4467927ad2668ca434a",
+    "url": "/austinPortfolio/static/js/main.2f901ec8.chunk.js"
+  },
+  {
+    "revision": "f6417a9fc88e33a0598c",
+    "url": "/austinPortfolio/static/js/runtime~main.98211d1e.js"
+  },
+  {
+    "revision": "2803a0e6de100b64d311fb539f693e51",
+    "url": "/austinPortfolio/static/media/CompImg.2803a0e6.png"
+  },
+  {
+    "revision": "89a1d495e3ea315d9fa223548a9d9afe",
+    "url": "/austinPortfolio/static/media/Florentia-Regular-trial.89a1d495.ttf"
+  },
+  {
+    "revision": "dfba97e55a0ca5d85e9be75227f4d586",
+    "url": "/austinPortfolio/static/media/Jhiaxus.dfba97e5.otf"
+  },
+  {
+    "revision": "502382a972588d19dd34d2c195c40f7c",
+    "url": "/austinPortfolio/static/media/NCCFLogo.502382a9.jpg"
+  },
+  {
+    "revision": "0bf7ad3bc28d0f0ac015577ec72b6b06",
+    "url": "/austinPortfolio/static/media/TOCHLogo.0bf7ad3b.jpg"
+  },
+  {
+    "revision": "01b39e64ca6f3c7a7fb6e5570814b220",
+    "url": "/austinPortfolio/static/media/busAppLogo.01b39e64.jpg"
+  },
+  {
+    "revision": "bbd75bff6314b9a7c189eb6d25b4a041",
+    "url": "/austinPortfolio/static/media/campusYLogo.bbd75bff.jpg"
+  },
+  {
+    "revision": "e5e88ea4cd9dae380f4151db8a89c105",
+    "url": "/austinPortfolio/static/media/socIssLogo.e5e88ea4.jpg"
+  },
+  {
+    "revision": "d01c9bd3d0e5128f5c33ff1599d13f0f",
+    "url": "/austinPortfolio/static/media/usabilityTest.d01c9bd3.pdf"
+  },
+  {
+    "revision": "2e39123a089a8146331590e0bbc3998a",
+    "url": "/austinPortfolio/static/media/vectorArtLogo.2e39123a.jpg"
+  }
+]);
