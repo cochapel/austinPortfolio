3fc11e9b1de85bc5fb2f7b906fbe08430380051c
Austin Phillips <cochapel@live.unc.edu>
2019-08-29 17:15:15 -0400

    Fixed URL

---
+++ precache-manifest.fad130cbe25b63163bcb2dc52405bdf3.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb33c4dd981d29b4c2ce4cc41e29de15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6f2373ad472913e79e1",
+    "url": "/static/css/main.6be6f661.chunk.css"
+  },
+  {
+    "revision": "b1de7f6aba44fd265908",
+    "url": "/static/js/2.fe37a2b1.chunk.js"
+  },
+  {
+    "revision": "a6f2373ad472913e79e1",
+    "url": "/static/js/main.cab56e50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0acca860846d774ed2ca97f7d03eea30",
+    "url": "/static/media/CampusYUserJourney.0acca860.png"
+  },
+  {
+    "revision": "67a5d5e74f7ba94d96a082d43cb9d378",
+    "url": "/static/media/CampusYUserJourney2.67a5d5e7.png"
+  },
+  {
+    "revision": "89a1d495e3ea315d9fa223548a9d9afe",
+    "url": "/static/media/Florentia-Regular-trial.89a1d495.ttf"
+  },
+  {
+    "revision": "dfba97e55a0ca5d85e9be75227f4d586",
+    "url": "/static/media/Jhiaxus.dfba97e5.otf"
+  },
+  {
+    "revision": "502382a972588d19dd34d2c195c40f7c",
+    "url": "/static/media/NCCFLogo.502382a9.jpg"
+  },
+  {
+    "revision": "42e5a95c1f93964feee25e872dd97c1d",
+    "url": "/static/media/Phillips_final.42e5a95c.png"
+  },
+  {
+    "revision": "ee656b37e0ccb23a103ce04b8ccdacd8",
+    "url": "/static/media/Phillips_wireframe.ee656b37.png"
+  },
+  {
+    "revision": "0719bbdfdb594141f7d5a9d2339df569",
+    "url": "/static/media/audienceAnalysis.0719bbdf.pdf"
+  },
+  {
+    "revision": "01b39e64ca6f3c7a7fb6e5570814b220",
+    "url": "/static/media/busAppLogo.01b39e64.jpg"
+  },
+  {
+    "revision": "bbd75bff6314b9a7c189eb6d25b4a041",
+    "url": "/static/media/campusYLogo.bbd75bff.jpg"
+  },
+  {
+    "revision": "2a5c6fd37f7e2355afd6d01485c88ec5",
+    "url": "/static/media/committees.2a5c6fd3.png"
+  },
+  {
+    "revision": "3812f2192d14ccf85508fa903168bb5d",
+    "url": "/static/media/committees.3812f219.jpg"
+  },
+  {
+    "revision": "b3cd1a3e76377c769cbcf7d9688857b1",
+    "url": "/static/media/finHomepage.b3cd1a3e.png"
+  },
+  {
+    "revision": "13f3fe11e3c38a7dda3e4a61ffc7757b",
+    "url": "/static/media/homepage.13f3fe11.png"
+  },
+  {
+    "revision": "2e64f0803dbb2ea9c7252b7717575932",
+    "url": "/static/media/iter1.2e64f080.jpg"
+  },
+  {
+    "revision": "447805585def612eca06b66b93c0dbdb",
+    "url": "/static/media/iter1.44780558.jpg"
+  },
+  {
+    "revision": "2966efbedb4d55ff6751295c8a734667",
+    "url": "/static/media/iter2-2.2966efbe.jpg"
+  },
+  {
+    "revision": "773dad6a207253787d3f6db91e39f80f",
+    "url": "/static/media/iter2.773dad6a.jpg"
+  },
+  {
+    "revision": "78f21c3762926abe02fc4f028d12d0bc",
+    "url": "/static/media/iter22.78f21c37.jpg"
+  },
+  {
+    "revision": "1c3fb102766afbdf7c47d11ddac92c58",
+    "url": "/static/media/iter23.1c3fb102.jpg"
+  },
+  {
+    "revision": "2afd0daec9d0e3d79b77534d3454596b",
+    "url": "/static/media/iter24.2afd0dae.jpg"
+  },
+  {
+    "revision": "91670557c5e9b3001b902396f6607b14",
+    "url": "/static/media/iter25.91670557.jpg"
+  },
+  {
+    "revision": "191122dd6d26a0e19da30acf23526fb8",
+    "url": "/static/media/iter32.191122dd.jpg"
+  },
+  {
+    "revision": "73bd8a3d04bb553a0e83375c8fa57cc5",
+    "url": "/static/media/iter33.73bd8a3d.jpg"
+  },
+  {
+    "revision": "c79cea11f533cb3d707ad57714b3281c",
+    "url": "/static/media/iter34.c79cea11.jpg"
+  },
+  {
+    "revision": "7be5edb452d0113888a6f556b2e44bad",
+    "url": "/static/media/iter35.7be5edb4.jpg"
+  },
+  {
+    "revision": "fabd3ce3e5688e215725a821c9aca149",
+    "url": "/static/media/myLogo.fabd3ce3.jpg"
+  },
+  {
+    "revision": "fa5059fa48e251680aa3ccc018926fd3",
+    "url": "/static/media/myWire.fa5059fa.jpg"
+  },
+  {
+    "revision": "618ccc5ada4c78ab615d4bddfbf01d0d",
+    "url": "/static/media/nccfPlat.618ccc5a.png"
+  },
+  {
+    "revision": "4ac69fec042286daf5102ae0a903a49e",
+    "url": "/static/media/newsletter.4ac69fec.jpg"
+  },
+  {
+    "revision": "fc77908d5ebf5971cf8f0445785bedf8",
+    "url": "/static/media/newsletter.fc77908d.png"
+  },
+  {
+    "revision": "4149cd4868e6a48d01d865e40da02c4a",
+    "url": "/static/media/persona1.4149cd48.png"
+  },
+  {
+    "revision": "28f1aa621d3111892aa6a859248d040b",
+    "url": "/static/media/persona2.28f1aa62.png"
+  },
+  {
+    "revision": "e73f2ff9357e7930b41076593f1198af",
+    "url": "/static/media/phillips_infographic3.e73f2ff9.png"
+  },
+  {
+    "revision": "10c4f7e41d893944a5cbf387d36adc62",
+    "url": "/static/media/portExamp.10c4f7e4.png"
+  },
+  {
+    "revision": "63189dd2a0b3a9d242816d7b1b4d9598",
+    "url": "/static/media/portProject_desktop.63189dd2.jpg"
+  },
+  {
+    "revision": "21882b583519ae69eab1b8e6d3d7ceb8",
+    "url": "/static/media/portProject_mobile.21882b58.jpg"
+  },
+  {
+    "revision": "0f42c1a2922c5b29dc89a8f453133bdd",
+    "url": "/static/media/portStyleguide.0f42c1a2.png"
+  },
+  {
+    "revision": "f367da587f01e620484642b3425fefd5",
+    "url": "/static/media/reservation.f367da58.jpg"
+  },
+  {
+    "revision": "7a2bb05496eb2a88d646da3e08256953",
+    "url": "/static/media/roomReserv1.7a2bb054.png"
+  },
+  {
+    "revision": "0b10448ab2f7a50b229d76d4a03313bf",
+    "url": "/static/media/roomReserv2.0b10448a.png"
+  },
+  {
+    "revision": "6b06027d7268b2f715511c0c668cc4f1",
+    "url": "/static/media/roomReserv3.6b06027d.png"
+  },
+  {
+    "revision": "980c852092afbe2d17b5ae2f01a2a5ce",
+    "url": "/static/media/sketch1.980c8520.jpg"
+  },
+  {
+    "revision": "d8549d5968e239d7d0ec223ed13f05b9",
+    "url": "/static/media/sketch1.d8549d59.jpg"
+  },
+  {
+    "revision": "f76bb78cd6a15e046d0894110db4ecbe",
+    "url": "/static/media/sketch1.f76bb78c.jpeg"
+  },
+  {
+    "revision": "0dc2697559e795bf174d808b130de8c5",
+    "url": "/static/media/sketch2.0dc26975.jpg"
+  },
+  {
+    "revision": "49c20bece0e20e5e3c33ea9a3ed79ba2",
+    "url": "/static/media/sketch2.49c20bec.jpg"
+  },
+  {
+    "revision": "9ce828586d4fb3a63da47e37a6e9a212",
+    "url": "/static/media/sketch2.9ce82858.jpeg"
+  },
+  {
+    "revision": "02585e22af42c9d0123b637a67fddb6c",
+    "url": "/static/media/sketch3.02585e22.jpg"
+  },
+  {
+    "revision": "832b98b6f91110457144135560fdfaf0",
+    "url": "/static/media/sketch3.832b98b6.jpeg"
+  },
+  {
+    "revision": "ce842ceb4f3085e1b54cf3642afaa892",
+    "url": "/static/media/sketch3.ce842ceb.jpg"
+  },
+  {
+    "revision": "2776de50c429f543656ad17dd03394fa",
+    "url": "/static/media/sketch4.2776de50.jpg"
+  },
+  {
+    "revision": "aa00a5dd0f0348fbe9885580aff9fdd3",
+    "url": "/static/media/sketch5.aa00a5dd.jpg"
+  },
+  {
+    "revision": "e5e88ea4cd9dae380f4151db8a89c105",
+    "url": "/static/media/socIssLogo.e5e88ea4.jpg"
+  },
+  {
+    "revision": "691fe6484e2a1afc69618c6a1fab3901",
+    "url": "/static/media/usabilityReport.691fe648.pdf"
+  },
+  {
+    "revision": "d01c9bd3d0e5128f5c33ff1599d13f0f",
+    "url": "/static/media/usabilityTest.d01c9bd3.pdf"
+  },
+  {
+    "revision": "2e39123a089a8146331590e0bbc3998a",
+    "url": "/static/media/vectorArtLogo.2e39123a.jpg"
+  },
+  {
+    "revision": "7508974bde81956eee36da1b6be16388",
+    "url": "/static/media/wireframe_desktop.7508974b.png"
+  },
+  {
+    "revision": "561a602272219147c9f0c92357f7b543",
+    "url": "/static/media/wireframe_desktop2.561a6022.png"
+  },
+  {
+    "revision": "3660273f05e0eae49b501fadf4d6e7cb",
+    "url": "/static/media/wireframe_mobile.3660273f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/austinPortfolio/precache-manifest.b73a8314cfe7c3b5f7c93fb833c36551.js"
+  "/precache-manifest.fad130cbe25b63163bcb2dc52405bdf3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/austinPortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
